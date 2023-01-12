c2986254071ca6a1db09960c708f2726ad4dceeb
Kiana Bayani Nekoudin <72384773+alu0101318066@users.noreply.github.com>
2023-01-12 11:32:27 +0000

    Cambios

--- pages/index.js
+++ pages/index.js
@@ -2,60 +2,10 @@ import Head from "next/head";
 import { useState } from "react";
 import styles from "./index.module.css";
 
-//Código para las imágenes
-/*export default function Home() {
-  const [promptInput, setPrompt] = useState("");
-  const [result, setResult] = useState("");
-
-
-  async function onSubmit(event) {
-    event.preventDefault();
-    const response = await fetch("/api/image", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ prompt: promptInput }),
-    });
-    const data = await response.json();
-    setResult(data.result);
-    setPrompt("");
-  };
-
-  return (
-    <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/photo_icon.png" />
-      </Head>
-
-      <main className={styles.main}>
-        <img src="/photo_icon.png" className={styles.icon} />
-        <h3>Photo generator</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Ej. Green horse with purple eyes"
-            value={promptInput}
-            onChange={(e) => setPrompt(e.target.value)}
-          />
-          <input type="submit" value="Generate image" />
-        </form>
-        <br></br>
-        <img className="result-image" src={result} />
-      </main>
-    </div>
-  );
-
-}*/
-
-
-// Código para los nombres
 export default function Home() {
-  // Write the code
   const [animalInput, setAnimalInput] = useState("");
   const [result, setResult] = useState();
+
   async function onSubmit(event) {
     event.preventDefault();
     const response = await fetch("/api/generate", {
@@ -75,6 +25,7 @@ export default function Home() {
         <title>OpenAI Quickstart</title>
         <link rel="icon" href="/dog.png" />
       </Head>
+
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
         <h3>Name my pet</h3>
