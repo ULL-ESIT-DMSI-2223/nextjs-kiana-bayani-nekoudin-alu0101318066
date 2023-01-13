14c696739452727199223c435794056e6aa8318d
Kiana Bayani Nekoudin <72384773+alu0101318066@users.noreply.github.com>
2023-01-13 14:36:33 +0000

    Cambios

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,30 +1,31 @@
-import Layout from '../../components/layout';
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
 
-export default function Post() {
-  return <Layout>
-    {PostData.title}
-    <br />
-    {postData.id}
-    <br />
-    {postData.date}
-  </Layout>;
-}
-
-import { getAllPostIds, getPostData } from '../../lib/posts';
-
-export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
-  return {
-    props: {
-      postData,
-    },
-  };
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds();
+  const paths = getAllPostIds()
   return {
     paths,
-    fallback: false,
-  };
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
 }
